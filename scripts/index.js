2dbfc9e81dcf2318f56c571fe3c672ab879e9546
AmandaRose <manda.holguin@gmail.com>
2025-11-02 15:50:01 -0700

    New project update

--- scripts/index.js
+++ scripts/index.js
@@ -47,29 +47,42 @@ const profileDescriptionEl = document.querySelector(".profile__description");
 const addImageLinkEl = document.querySelector("#card-image-input");
 const addImageCaptionEl = document.querySelector("#card-caption-input");
 
+function openModal(modal) {
+  modal.classList.add("modal_is-opened");
+}
+
+function closeModal(modal) {
+  modal.classList.remove("modal_is-opened");
+}
+
 editProfileBtn.addEventListener("click", function () {
   editProfileNameInput.value = profileNameEl.textContent;
   editProfileDescriptionInput.value = profileDescriptionEl.textContent;
-  editProfileModal.classList.add("modal_is-opened");
+  /* editProfileModal.classList.add("modal_is-opened"); */
+  openModal(editProfileModal);
 });
 
 editProfileCloseBtn.addEventListener("click", function () {
-  editProfileModal.classList.remove("modal_is-opened");
+  /* editProfileModal.classList.remove("modal_is-opened"); */
+  closeModal(editProfileModal);
 });
 
 newPostBtn.addEventListener("click", function () {
-  newPostModal.classList.add("modal_is-opened");
+  /* newPostModal.classList.add("modal_is-opened"); */
+  openModal(newPostModal);
 });
 
 newPostCloseBtn.addEventListener("click", function () {
-  newPostModal.classList.remove("modal_is-opened");
+  /* newPostModal.classList.remove("modal_is-opened"); */
+  closeModal(newPostModal);
 });
 
 function handleEditProfileSubmit(evt) {
   evt.preventDefault();
   profileNameEl.textContent = editProfileNameInput.value;
   profileDescriptionEl.textContent = editProfileDescriptionInput.value;
-  editProfileModal.classList.remove("modal_is-opened");
+  /* editProfileModal.classList.remove("modal_is-opened"); */
+  closeModal(editProfileModal);
   console.log("submitting");
 }
 
@@ -78,7 +91,8 @@ editProfileForm.addEventListener("submit", handleEditProfileSubmit);
 function handleAddCardSubmit(evt) {
   evt.preventDefault();
   console.log(addImageLinkEl.value, addImageCaptionEl.value);
-  addNewPostModal.classList.remove("modal_is-opened");
+  /* addNewPostModal.classList.remove("modal_is-opened"); */
+  closeModal(addNewPostModal);
 }
 
 newPostModal.addEventListener("submit", handleAddCardSubmit);
